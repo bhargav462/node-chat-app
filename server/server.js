84b6d54a03681b0b7ebd28adaf84a6a1f419403a
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2019-11-02 18:09:33 +0530

    Connection and Disconnetion with Event listeners

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.set('view engine','hbs');
 app.use(express.static(publicPath));
+
+io.on('connection',(socket) => {
+    console.log('New user connection');
+
+    socket.on('disconnect',() => {
+        console.log('User was Disconnected');
+    })
+});
 
 // app.post('/',(req,res) => {
 //  res.render('index.html');
 // });
 
-app.listen(port,() =>{
+server.listen(port,() =>{
     console.log(`server is up on port ${port}`);
 })
