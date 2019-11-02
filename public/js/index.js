47a8f9c063fd89b9cbf51736101511b567d0b957
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2019-11-02 22:39:54 +0530

    add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -7,11 +7,33 @@ socket.on('connect',function(){
   });
 });
 
-socket.on('newMessage',function(newMessage){
-    console.log('newMessage',newMessage);
+// socket.emit('createMessage',{
+//     from:"me",
+//     text:"test"
+// },function(data){
+//     console.log('nothing',data);
+// })
+
+socket.on('newMessage',function(message){
+    console.log('newMessage',message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    $('#messages').append(li);
 });
 
 socket.on('disconnect',function(){
     console.log('Disconnected');
+});
+
+jQuery('#message-form').on('submit',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:'user',
+        text:$('[name=message]').val()
+    },function(){
+         
+    })
 });
 
